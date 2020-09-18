cd604998296c70d04dbb5752806f174d23e35b5a
Andres Nuila <enuila634@gmail.com>
2020-09-18 16:57:37 -0600

    avance

--- main.cpp
+++ main.cpp
@@ -1,8 +1,54 @@
 #include <iostream>
-
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <string>
 using namespace std;
 
-int main()
+int main(int argc, char *argv[])
 {
-    cout << "hola mundo";
+    string directorioArchivo = "./";
+    directorioArchivo += argv[1];
+    ifstream read;
+    read.open(directorioArchivo, ios::in);
+    vector<string> clases;
+    vector<string> atributos;
+    vector<string> _claseIndividual;
+    while (!read.eof())
+    {
+        string clase;
+        getline(read, clase);
+        stringstream tokens(clase);
+        string separacion;
+        while (getline(tokens, separacion, ']'))
+        {
+            clases.push_back(separacion);
+        }
+    }
+    for (int i = 0; i < clases.size(); i++)
+    {
+        string claseAUsar = clases.at(i);
+        string atributos_por_linea = "";
+        stringstream tokensAtributos(claseAUsar);
+        while (getline(tokensAtributos, atributos_por_linea, ' '))
+        {
+            atributos.push_back(atributos_por_linea);
+        }
+        for (int j = 0; j < atributos.size(); j++)
+        {
+            string atributoAUsar = atributos.at(j);
+            string atributos_individuales = "";
+            stringstream separarAtributoEnEspecifico(atributoAUsar);
+            while (getline(separarAtributoEnEspecifico, atributos_individuales, ':'))
+            {
+                _claseIndividual.push_back(atributos_individuales);
+            }
+        }
+        for (int k = 0; k < _claseIndividual.size(); k++)
+        {
+            cout << _claseIndividual.at(k) << "--";
+        }
+    }
+
+    read.close();
 }
